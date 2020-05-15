3b99a2143780e4fc7ccd21efe2f2333b09b7bd24
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2020-05-15 10:43:28 -0400

    Compile package so i dont lose my mind

--- amazon-ecs-get-task-definition/index.js
+++ amazon-ecs-get-task-definition/index.js
@@ -29,6 +29,8 @@ async function run() {
       throw(error);
     }
 
+    core.debug("Response:");
+    core.debug(describeResponse);
     const containerDef = describeResponse.taskDefinition.containerDefinitions.find(function(element) {
       return element.name == containerName;
     });
